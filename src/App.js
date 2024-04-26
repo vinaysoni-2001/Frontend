bbfff2c6267ee18a8844fe1e849c5ef63eb7292d
Anuj <anujsde@gmail.com>
2024-04-26 18:04:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import Login from './component/Login/Login';
+import Signup from './component/Signup/Signup';
+import Header from './component/Header';
+import Admin from './component/Admin/Admin';
+import ProtectedRoute from './component/ProtectedRoute'; // New component for protected routes
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/admin"
+          element={<ProtectedRoute element={<Admin />} />}
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/component/Admin/Admin.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../Header";
+import Dashboard from "../Dashboard/Dashboard";
+
+const Admin = () => {
+    return (
+        <>
+        <Header />
+        <Dashboard />
+        </>
+    )
+}
+
+
+export default Admin;
